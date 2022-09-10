5b9159630a88837fec4e9d4f7f23dc3ed78c0348
gibson123456 <egedegbegodwin7@gmail.com>
2022-09-10 05:43:25 -0700

    Love

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 /**
- *
- *  * main - main function
- *
- *   *
- *
- *    * Return: end program
- *
- *     */
+
+  * main - main function
+ 
+  *
+  * Return: end program
+
+*/
 
 int main(void)
 
   {
 
 		char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
 		fprintf(stderr, "%s\n", n);
+
 			return (1);
 
   }
