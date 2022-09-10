d6f8897b8d8cf4518790c9b2d46105f076632952
gibson123456 <egedegbegodwin7@gmail.com>
2022-09-10 06:15:18 -0700

    Test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,13 @@
 #include <stdio.h>
-
 /**
+ * main - entry point
  *
- * * main - entry point
- *
- * *
- *
- * * Return: 0 number
- *
- * */
-
+ * Return: 0 number
+ */
 int main(void)
-	
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
-
+	
 	return (0);
-
 }
