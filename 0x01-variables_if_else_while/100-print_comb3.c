60f9e4595b56c9e39b08b7fce551307b082527c3
gibson123456 <egedegbegodwin7@gmail.com>
2022-09-11 15:39:59 -0700

    Good Life

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 48; a < 58; a++)
+	{
+		for (b = 48; b < 58; b++)
+		{
+			if (a != b && b > a)
+			{
+				putchar(a);
+				putchar(b);
+				if (b == 57 && a == 56}
+				{
+				break
+					;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
