7336e9d80335d647429a31c02fb93acb728ee7c7
gibson123456 <egedegbegodwin7@gmail.com>
2022-09-11 12:54:54 -0700

    Mercy

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,19 +6,17 @@
  *
  * Return: Always return 0 (Success)
  */
-
 int main(void)
-
 {
 	char alp, e, q;
-		
+
 	e = 'e';
 	q = 'q';
 	for (alp = 'a'; alp <= 'z'; alp++)	
 	{
 		if (alp != e && alp != q)
-			putchar(alp);
-	}	
+		putchar(alp);
+	}
 	putchar('\n');
 	return (0);
 	}
