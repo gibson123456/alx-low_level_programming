0b30fe19bef4aa99863ca18a8fa19f13be6093cc
gibson123456 <egedegbegodwin7@gmail.com>
2022-09-14 08:00:35 -0700

    Life

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,15 +3,14 @@
  * print_alphabet - prints the alphabets in lower case
  * Return: returns 0 if successful
  */
-void  print_alphabet(void)
+void print_alphabet(void)
 {
-	char alphabet = 'a';
+char alphabet = 'a';
 
-	while (alphabet <= 'z')
-	{
-		_putchar(alphabet);
-		alphabet++;
-	}
-	_putchar('\n');
+while (alphabet <= 'z')
+{
+_putchar(alphabet);
+alphabet++;
 }
-	
+_putchar('\n');
+}
