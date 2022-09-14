f2c3b0be486c70e823797e36431bced925bb3522
gibson123456 <egedegbegodwin7@gmail.com>
2022-09-14 03:53:49 -0700

    Intro

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,15 @@
  */
 int main(void)
 {
-	char string[8] = "_putchar";
-	int n = 0;
+	_putchar ('_');
+	_putchar ('p');
+	_putchar ('u');
+	_putchar ('t');
+	_putchar ('c');
+	_putchar ('h');
+	_putchar ('a');
+	_putchar ('r');
+	_putchar ('\n');
 
-	while (n < 8)
-	{
-		_putchar(string[n]);
-		++n; 
-	}
-	_putchar(10);
 	return(0);
 }
