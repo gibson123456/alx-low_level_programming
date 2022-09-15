c452a60511b47bffe0fdd15b86a2ba1825bf7d33
gibson123456 <egedegbegodwin7@gmail.com>
2022-09-15 15:31:03 -0700

    Win

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HEADER_FILE
+#define HEADER_FILE
 
 
 int _putchar(char c);
+
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
