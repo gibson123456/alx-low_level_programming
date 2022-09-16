a5b793f9f7a444d68d4fb80323e5240e205eaf65
gibson123456 <egedegbegodwin7@gmail.com>
2022-09-16 01:59:15 -0700

    Win

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,12 +1,11 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
-
 int _putchar(char c);
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
-#endif 
+#endif
