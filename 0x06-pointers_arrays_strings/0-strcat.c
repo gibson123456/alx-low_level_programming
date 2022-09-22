42b6f3722e83301bd613b3307b0bdf552de39feb
gibson123456 <egedegbegodwin7@gmail.com>
2022-09-22 06:43:17 -0700

    Greatness is Sure

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,26 +1,26 @@
 #include "main.h"
 /**
- * _strcat - concatenates two strings
- *
- *@dest: string to append to
- *@src: string to add
- *
- * Return: a pointer to the resulting string
+ * strcat - concatenates two strings
+ * @dest: A pointer to a character that will be changed
+ * @src: A pointer to a character that will also be changed
+ * Return: dest
  */
 char *_strcat(char *dest, char *src)
 {
-	int x = 0;
-	int y = 0;
+	int i, j;
 
-	while (dest[x])
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		x++;
+		i++;
 	}
-	while (src[y])
+	j = 0;
+	while (src[j] != '\0')
 	{
-		dest[x] = src[y];
-		x++;
-		y++;
+		dest[i] = src[j];
+		j++;
+		i++;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
