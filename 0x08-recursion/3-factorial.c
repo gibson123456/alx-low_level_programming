bb14bc8d2d810949f3f7e6530ecfd522f3c84341
gibson123456 <egedegbegodwin7@gmail.com>
2022-09-28 14:05:52 -0700

    You mustn't be afraid to dream a little bigger, darling

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- * factorial - returns the factorial of a given number
- * @n: integer to take factorial of
- * 
- * Return: factorial of n, or -1 if n < 0
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: base number
+ * @y: exponent
+ *
+ * Return: x ^ y or -1 if y < 0
  */
-int factorial(int n)
+int _pow_recursion(int x, int y)
 {
-	if (n < 0)
+	if (y < 0)
 		return (-1);
-	if (n == 0)
+	if (y == 0)
 		return (1);
-	return (n * factorial(n - 1));
+	return (x * _pow_recursion(x, y - 1));
 }
