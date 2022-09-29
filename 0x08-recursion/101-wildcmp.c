a5721b49f35cb5f08677801cfcbd00d4bf76be7a
gibson123456 <egedegbegodwin7@gmail.com>
2022-09-29 13:44:44 -0700

    Machine

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -23,5 +23,5 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
-	return (0);}
+	return (0);
 }
--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,15 +1,18 @@
 #include "main.h"
 /**
  * _pow_recursion - returns the value of x raised to the power of y
- * @x: base number
- * @y: exponent
- *
- * Return: x ^ y or -1 if y < 0
+ * @x: number to be squared
+ * @y: power of x to be squared
+ * Return: result of x**y
  */
 int _pow_recursion(int x, int y)
 {
-	if (y < 0)
-		return (-1);
-	if (y == 0)
+	if (y >= 0)
+	{
+		if (y == 0)
+			return (1);
 		return (x * _pow_recursion(x, y - 1));
+	}
+	else
+		return (-1);
 }
