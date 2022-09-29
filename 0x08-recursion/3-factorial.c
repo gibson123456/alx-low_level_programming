bd36895396bf21b978a7bf11f65375875ea52050
gibson123456 <egedegbegodwin7@gmail.com>
2022-09-29 13:50:31 -0700

    Wealth

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,16 +1,14 @@
 #include "main.h"
 /**
  * factorial - returns the factorial of a given number
- * @n: integer to take factorial of
- *
- *
- * Return: factorial of n, or -1 if n < 0
+ * @n: input number
+ * Return: factorial of input
  */
 int factorial(int n)
 {
 	if (n < 0)
-		return (-1)
-	if (n == 0)
+		return (-1);
+	if (n == 1)
 		return (1);
 	return (n * factorial(n - 1));
 }
